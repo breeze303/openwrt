12009ad5d632a0b6281fec7ad52986ee635ff5a5
breeze303 <grapenskrskr@gmail.com>
2025-08-30 22:08:45 +0800

    airoha: add gemtek-w1701k

--- target/linux/airoha/image/an7581.mk
+++ target/linux/airoha/image/an7581.mk
@@ -1,3 +1,9 @@
+define Device/FitImage
+	KERNEL_SUFFIX := -uImage.itb
+	KERNEL = kernel-bin | libdeflate-gzip | fit gzip $$(KDIR)/image-$$(DEVICE_DTS).dtb
+	KERNEL_NAME := Image
+endef
+
 define Device/FitImageLzma
 	KERNEL_SUFFIX := -uImage.itb
 	KERNEL = kernel-bin | lzma | fit lzma $$(KDIR)/image-$$(DEVICE_DTS).dtb
@@ -5,7 +11,7 @@ define Device/FitImageLzma
 endef
 
 define Device/airoha_an7581-evb
-  $(call Device/FitImageLzma)
+  $(call Device/FitImage)
   DEVICE_VENDOR := Airoha
   DEVICE_MODEL := AN7581 Evaluation Board (SNAND)
   DEVICE_PACKAGES := kmod-leds-pwm kmod-i2c-an7581 kmod-pwm-airoha kmod-input-gpio-keys-polled
@@ -27,7 +33,7 @@ endef
 TARGET_DEVICES += airoha_an7581-evb-emmc
 
 define Device/gemtek_w1700k
-  $(call Device/FitImageLzma)
+  $(call Device/FitImage)
   DEVICE_VENDOR := Gemtek
   DEVICE_MODEL := W1700K
   DEVICE_ALT0_VENDOR := CenturyLink
@@ -40,7 +46,28 @@ define Device/gemtek_w1700k
   DEVICE_DTS_DIR := ../dts
   DEVICE_PACKAGES := kmod-i2c-an7581 kmod-pwm-an7581 \
 		    kmod-mt7996-firmware
+  KERNEL_SIZE := 8192k
   KERNEL_LOADADDR := 0x80088000
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += gemtek_w1700k
+
+define Device/gemtek_w1701k
+  $(call Device/FitImage)
+  DEVICE_VENDOR := Gemtek
+  DEVICE_MODEL := W1701K
+  DEVICE_ALT0_VENDOR := CenturyLink
+  DEVICE_ALT0_MODEL := W1701K
+  DEVICE_ALT1_VENDOR := Lumen
+  DEVICE_ALT1_MODEL := W1701K
+  DEVICE_ALT2_VENDOR := Quantum Fiber
+  DEVICE_ALT2_MODEL := W1701K
+  DEVICE_DTS := an7581-gemtek-w1701k
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := kmod-i2c-an7581 kmod-pwm-an7581 \
+		    kmod-mt7996-firmware
+  KERNEL_SIZE := 8192k
+  KERNEL_LOADADDR := 0x80088000
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += gemtek_w1701k
